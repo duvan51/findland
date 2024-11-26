b4c57a2f7a989f38ec0d74e61f0ec830e44f4798
duvan51 <duvanaponteramirez@gmail.com>
2024-11-26 02:36:00 -0500

    actualizacion de login y fusion ramaFront

--- client/src/components/ui/Navbar.jsx
+++ client/src/components/ui/Navbar.jsx
@@ -5,8 +5,11 @@ import { IoChevronBackSharp } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <div className="">
-      <Link to='/'><IoChevronBackSharp /><h1>Brand & Logo</h1></Link>
+    <div className="px-10">
+      <Link to='/' className="flex ">
+        <div className="text-5xl"><IoChevronBackSharp /></div>
+        <h1 className="flex items-center">Volver al inicio</h1>
+      </Link>
     </div>
   )
 }
